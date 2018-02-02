1f75d95ead3bcc9a129cccc6be2472c76782766e
Benjamin Liden <benjamin.c.liden@gmail.com>
2018-02-02 16:18:52 -0500

    selectors added && reset message set

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-document.getElementById('cutForm').addEventListener('submit', (e) => {
-    console.log('successfully intercepted');
+const form = document.querySelector('form');
+const textArea = form.querySelector('textArea[name="textArea"]').value;
+const chunkSize = form.querySelector('select[name="chunkSelect"]').value;
+const reverseBool = form.querySelector('input[name="reverseBox"]').checked;
+const shuffleBool = form.querySelector('input[name="shuffleBox"]').checked;
+const resultArea = document.querySelector('div.resultsDiv p');
+
+form.addEventListener('submit', (e) => {
     e.preventDefault();
 });
+
+console.log('Shuffle: ', shuffleBool);
+console.log('Reverse: ', reverseBool);
+console.log('textArea: ', textArea);
+console.log('chunkSize: ', chunkSize);
+
+
+
+form.addEventListener('reset', () => {
+    resultArea.textContent = 'Your results will appear here.';
+});
+
+// resultArea.textContent = 'the script ran';
