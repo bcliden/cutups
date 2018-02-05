9fcf572665eb0c30278371f19d1f5c817bf437f5
Benjamin Liden <benjamin.c.liden@gmail.com>
2018-02-05 18:10:25 -0500

    fixed selectors & some function work

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 const form = document.querySelector('form');
-const textArea = form.querySelector('textArea[name="textArea"]').value;
-const chunkSize = form.querySelector('select[name="chunkSelect"]').value;
-const reverseBool = form.querySelector('input[name="reverseBox"]').checked;
-const shuffleBool = form.querySelector('input[name="shuffleBox"]').checked;
+const textArea = form.querySelector('#textArea');
+const chunkSize = form.querySelector('select[name="chunkSelect"]');
+const reverseBool = form.querySelector('input[name="reverseBox"]');
+const shuffleBool = form.querySelector('input[name="shuffleBox"]');
 const resultArea = document.querySelector('div.resultsDiv p');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    let config = {
+        text: textArea.value,
+        chunk: chunkSize.value,
+        reverse: reverseBool.checked,
+        shuffle: shuffleBool.checked,
+    };
+    handleCut(config);
 });
-
-console.log('Shuffle: ', shuffleBool);
-console.log('Reverse: ', reverseBool);
-console.log('textArea: ', textArea);
-console.log('chunkSize: ', chunkSize);
-
-
 
 form.addEventListener('reset', () => {
     resultArea.textContent = 'Your results will appear here.';
 });
+
+function handleCut(config) {
+    console.dir(config.text);
+    console.log(config.chunk);
+    console.log(config.reverse);
+    console.log(config.shuffle);
+}
 
 // resultArea.textContent = 'the script ran';
